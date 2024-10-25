f0c28b27aa645b65413ebc9e1cba296b5a0038bb
yebishe4150 <mak.xim76@gmail.com>
2024-10-25 12:30:49 +0300

    4th

--- src/main/java/crud/config/AppConfig.java
+++ src/main/java/crud/config/AppConfig.java
@@ -1,7 +1,5 @@
 package crud.config;
 
-
-import crud.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
--- src/main/java/crud/service/UserServiceImpl.java
+++ src/main/java/crud/service/UserServiceImpl.java
@@ -1,74 +1,50 @@
 package crud.service;
 
-import crud.dao.UserDao;
 import crud.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserDao userDao;
-
-    @Autowired
-    public UserServiceImpl (UserDao userDao){
-        this.userDao = userDao;
-    }
-
-    @Transactional
-    @Override
-    public List<User> getAllUsers() {
-        return List.of();
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Transactional
     @Override
     public void saveUser(User user) {
-        userDao.saveUser(user);
+        entityManager.persist(user);
     }
 
     @Transactional
     @Override
     public User getById(long id) {
-        return userDao.getById(id);
+        return entityManager.find(User.class,id);
     }
 
+    @Transactional
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User",User.class).getResultList();
+    }
+
+    @Transactional
     @Override
     public void updateUser(User user) {
-        userDao.updateUser(user);
+        entityManager.merge(user);
     }
 
     @Transactional
     @Override
     public void deleteUser(long id) {
-        userDao.deleteUser(id);
+        User user = entityManager.find(User.class,id);
+        if (user != null){
+            entityManager.remove(user);
+        }
     }
-
-
-
-//    @PersistenceContext
-//    private EntityManager entityManager;
-//
-//    public List<User> getAllUsers() {
-//        return entityManager.createQuery("from User", User.class).getResultList();
-//    }
-//
-//    public void saveUser(User user) {
-//        entityManager.persist(user);
-//    }
-//
-//    public void updateUser(User user) {
-//        entityManager.merge(user);
-//    }
-//
-//    public void deleteUser(long id) {
-//        User user = entityManager.find(User.class, id);
-//        if (user != null) {
-//            entityManager.remove(user);
-//        }
-//    }
 }
 
